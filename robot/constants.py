863899f6f25b3732eaeb535abb4540f2f66ace21
DonggeonKim1012 <dgk1012dgk@gmail.com>
2025-12-20 20:33:26 +0900

    changed dataset dir

--- robot/constants.py
+++ robot/constants.py
@@ -2,7 +2,8 @@ import pathlib
 import os
 
 ### Task parameters
-DATA_DIR = os.path.expanduser('/home/robros-ai/dg/IL_data/new')
+# DATA_DIR = os.path.expanduser('/home/robros-ai/dg/IL_data/new')
+DATA_DIR = os.path.expanduser('/workspace/dataset2')
 TASK_CONFIGS = {
     ### BLOCK SORT
     'dsr_block_collect':{
