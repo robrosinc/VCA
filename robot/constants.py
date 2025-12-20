c6b4a48a6f1aece89713291c0d4b8f4ed32b3f52
DonggeonKim1012 <dgk1012dgk@gmail.com>
2025-12-20 20:08:26 +0900

    remove useless library, update inference parameter

--- robot/constants.py
+++ robot/constants.py
@@ -69,7 +69,7 @@ TASK_CONFIGS = {
     },
     'hanoi2':{
         'dataset_dir': DATA_DIR,
-        'episode_len': 5000,
+        'episode_len': 3000,
         'train_ratio': 0.99,
         'camera_names': ['rhand_camera', 'head_camera'],
         'robot_id_list': ['dsr_r'],
