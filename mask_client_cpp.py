7718d28aa00ac54e96e510bb465c7786d187a2c6
DonggeonKim1012 <dgk1012dgk@gmail.com>
2026-02-23 06:44:48 +0900

    tidy up, change checkpoint name

--- mask_client_cpp.py
+++ mask_client_cpp.py
@@ -148,7 +148,7 @@ def main(args):
     ckpt_dir = args['ckpt_dir']
     # ckpt_path = os.path.join(ckpt_dir, 'policy_best.ckpt')
     # ckpt_path = os.path.join(ckpt_dir, 'policy_last.ckpt')
-    ckpt_path = os.path.join(ckpt_dir, 'policy_step_400000_seed_10.ckpt')
+    ckpt_path = os.path.join(ckpt_dir, 'blocksort_with_mask.ckpt')
     
     print('ckpt_path: ', ckpt_path)
     config_path = os.path.join(ckpt_dir, 'config.pkl')
